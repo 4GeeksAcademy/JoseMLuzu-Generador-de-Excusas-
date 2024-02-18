2c6240070a37537ff43da1fb63e1ca357effb33c
Jose Manuel Luzuriaga <157091527+JoseMLuzu@users.noreply.github.com>
2024-02-18 07:48:21 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,37 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generateExcuse();
+  });
   console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My bird", "Her fish"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was sleeping"
+  ];
+
+  let whoIndx = Math.floor(Math.random() * who.length);
+  let actionIndx = Math.floor(Math.random() * action.length);
+  let whatIndx = Math.floor(Math.random() * what.length);
+  let whenIndx = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIndx] +
+    " " +
+    action[actionIndx] +
+    " " +
+    what[whatIndx] +
+    " " +
+    when[whenIndx]
+  );
 };
